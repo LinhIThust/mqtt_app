70afb68522c1c636d1eedda331e877ebabb4ca41
Đồng Quang Linh (CTVT-TTQLTCLinhdq1CL-KUDPT) <linhdq1@vingroup.net>
2022-01-24 11:51:36 +0700

    base mqtt

---
+++ app/src/main/java/com/example/mqtt_smarthome/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.mqtt_smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG  = "xxx";
+    MqttAndroidClient client = null;
+    Button btTest;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btTest =findViewById(R.id.btTest);
+        btTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               publish();
+            }
+        });
+        Log.d(TAG, "onCreate: Hello");
+        String clientId = MqttClient.generateClientId();
+        client= new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void publish() {
+        String topic = "linh/test";
+        String payload = "the payload";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
